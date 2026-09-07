fd68d3b3d843a9774c93eb0be83a8af4919a9d96
Isha M <isha.madanu@gmail.com>
2026-09-06 23:30:57 -0400

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        #binary search among rows, check first and last elem to see if target could be in btwn them
+        topRow = 0
+        bottomRow = len(matrix) - 1 
+
+        while topRow <= bottomRow:
+            row = (topRow + bottomRow) // 2
+            if matrix[row][0] > target:
+                bottomRow = row - 1
+            elif matrix[row][-1] < target:
+                topRow = row + 1
+            else: #if it is, then binary search among that row, otherwise check row above or below it
+                break
+            
+        
+        if not (topRow <= bottomRow):
+            return False #no valid row is found
+
+
+        #binary search among valid range row
+        row = (topRow + bottomRow) // 2
+        low, high = 0, len(matrix[0]) - 1
+
+        while low <= high:
+            col = (low + high) // 2
+            if target > matrix[row][col]:
+                low = col + 1
+            elif target < matrix[row][col]:
+                high = col - 1
+            else:
+                return True
+
+        return False
